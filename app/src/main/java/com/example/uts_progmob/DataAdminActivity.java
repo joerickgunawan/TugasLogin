dd6dd0f096ab5d211277664d5063d452427e22bc
Willy Vercely <54306866+willyvercely@users.noreply.github.com>
2019-11-06 23:15:44 +0700

    Tugas Dialog KRS

--- app/src/main/java/com/example/uts_progmob/DataAdminActivity.java
+++ app/src/main/java/com/example/uts_progmob/DataAdminActivity.java
@@ -1,14 +1,61 @@
 package com.example.uts_progmob;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
 public class DataAdminActivity extends AppCompatActivity {
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.logout,menu);
+        return true;
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item){
+        switch (item.getItemId()){
+            case R.id.logoutBtn : /*Intent logoutIntent = new Intent(MenuAdmin.this, LoginScreen.class);
+            startActivity(logoutIntent);*/
+                AlertDialog.Builder builder = new AlertDialog.Builder(DataAdminActivity.this);
+                builder.setMessage("Apakah anda ingin Logout?").setNegativeButton("NO", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int which) {
+                        Toast.makeText(DataAdminActivity.this, "Tidak jadi LogOut", Toast.LENGTH_SHORT).show();
+                    }
+                })
+                        .setPositiveButton("YES", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int which) {
+                                Toast.makeText(DataAdminActivity.this, "Berhasil LogOut", Toast.LENGTH_SHORT).show();
+                                Intent intent = new Intent(DataAdminActivity.this, LogInActivity.class);
+                                startActivity(intent);
+                            }
+                        });
+                AlertDialog dialog = builder.create();
+                dialog.show();
+                return true;
+        }
+        return false;
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_data_admin);
+    }
+
+    public void GoToCRUDKRS(View view) {
+        Intent intent = new Intent(DataAdminActivity.this,InsertUpdateDataKRSActivity.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/uts_progmob/InsertUpdateDataKRSActivity.java
+++ app/src/main/java/com/example/uts_progmob/InsertUpdateDataKRSActivity.java
@@ -1,8 +1,14 @@
 package com.example.uts_progmob;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class InsertUpdateDataKRSActivity extends AppCompatActivity {
 
@@ -10,5 +16,32 @@ public class InsertUpdateDataKRSActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_insert_update_data_krs);
+
+        setTitle("ADMIN-KELOLA KRS");
+
+        Button simpanButton = (Button) findViewById(R.id.simpanBtn);
+        simpanButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(InsertUpdateDataKRSActivity.this);
+                builder.setMessage("Apakah ingin menyimpan data?").setNegativeButton("BATAL", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int which) {
+                        Toast.makeText(InsertUpdateDataKRSActivity.this,"Tidak jadi menyimpan", Toast.LENGTH_SHORT).show();
+                    }
+                })
+                        .setPositiveButton("YA", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int which) {
+                                Intent intent = new Intent(InsertUpdateDataKRSActivity.this, DataAdminActivity.class);
+                                startActivity(intent);
+                                Toast.makeText(InsertUpdateDataKRSActivity.this, "Berhasil Menyimpan", Toast.LENGTH_SHORT).show();
+                            }
+                        });
+                AlertDialog dialog = builder.create();
+                dialog.show();
+            }
+        });
     }
 }
+
